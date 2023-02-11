56aa8803676daf8494fe272191a80751618d0c6c
Rohan8017 <skr7959@gmail.com>
2023-02-11 18:09:53 +0530

    added Navbar

--- index.js
+++ index.js
@@ -16,6 +16,29 @@ const showHide2=document.querySelector('.show-hide2');
 const mainContent=document.querySelector('.main-content');
 const addNewDiv=document.getElementById('add-div');
 const checkList=document.querySelector('.check-list');
+const imageButton=document.querySelector('.image-button');
+const imageDropdown=document.querySelector('.image-dropdown');
+const imageInput=document.querySelector('#image-input');
+const uploadBtn = document.getElementById("upload-btn");
+const propertyBox=document.getElementById('propertyBox');
+const buttons =document.querySelectorAll('.buttons');
+buttons.forEach(button => {
+    button.addEventListener("mouseover", function(event) {
+        // console.log(button.type);
+    propertyBox.innerText = button.dataset.propertymsg;
+    const buttonRect = event.target.getBoundingClientRect();
+    if(button.type=='select-one'){
+        propertyBox.style.left = buttonRect.left + "px";
+    }else{
+        propertyBox.style.left = (buttonRect.left-30) + "px";
+    }
+    propertyBox.style.top = 50 +"px";
+    propertyBox.style.display = "block";
+    });
+    button.addEventListener("mouseleave", function() {
+        propertyBox.style.display = "none";
+    });
+});
 formatButtons.forEach((button)=>{
     button.addEventListener('click',()=>{
         button.classList.toggle('active');
@@ -45,18 +68,35 @@ checkList.addEventListener('click',()=>{
     div.appendChild(checkbox);
     textArea.appendChild(div);
 })
-spacingButtons.forEach((button)=>{
-    let indent=0;
-    if(button.classList.contains('increase-indent')){
-        button.addEventListener('click',()=>{
-            textArea.style.textIndent=`${indent+50}px`;
-        })
-    }else{  
-        button.addEventListener('click',()=>{
-            textArea.style.textIndent=`${indent-50}px`;
-        })
-    }
-})
+// spacingButtons.forEach((button)=>{
+//     let indent=0;
+//     if(button.classList.contains('increase-indent')){
+//         button.addEventListener('click',()=>{
+//             textArea.style.textIndent=`${indent+50}px`;
+//         })
+//     }else{  
+//         button.addEventListener('click',()=>{
+//             textArea.style.textIndent=`${indent-50}px`;
+//         })
+//     }
+// })
+uploadBtn.addEventListener("click", function () {
+    imageInput.click();
+  });
+
+  imageInput.addEventListener("change", function () {
+    const file = imageInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        const image=document.createElement('img');
+        image.src=reader.result;
+        textArea.appendChild(image);
+    });
+    reader.readAsDataURL(file);
+    imageDropdown.classList.add('show-edit');
+    document.querySelector('.image-button-div').classList.remove('active');
+  });
 function formatDoc(cmd, value = null) {
     // console.log(value);
     if (value) {
@@ -72,18 +112,6 @@ function addlink() {
     formatDoc('createLink', url);
     // document.execCommand('createLink',false,url);
 }
-textArea.addEventListener('mouseenter', function () {
-    const a = textArea.querySelectorAll('a');
-    a.forEach(item => {
-        item.addEventListener('mouseenter', function () {
-            textArea.setAttribute('contenteditable', false);
-            item.target = '_blank';
-        })
-        item.addEventListener('mouseleave', function () {
-            textArea.setAttribute('contenteditable', true);
-        })
-    })
-})
 function fontEditor(fontName) {
     document.execCommand("fontName", false, fontName);
 }
@@ -107,7 +135,6 @@ function fontSize(size) {
         }
         selectedHtml = container.innerHTML;
     }
-
     let html = `<div style="font-size: ${size}px;">${selectedHtml}</div>`
     document.execCommand('insertHTML', false, html);
 }
@@ -149,16 +176,36 @@ function changeFont() {
     const Font = document.getElementById('input-font').value;
     document.execCommand('fontName', false, Font);
 }
-function changeSize() {
-    const size = document.getElementById('fontSize').value;
-    document.execCommand('fontSize', false, size);
-}
+// fontSize.addEventListener("change", function() {
+//     const selected = window.getSelection().getRangeAt(0);
+//     const parent = selected.commonAncestorContainer;
+//     let node = selected.startContainer;
+//     // console.log(node)
+//     while (node !== parent) {
+//       node.style.fontSize = fontSize.value + "px";
+//       console.log(node.style.fontSize);
+//       node = node.parentNode;
+//     }
+// });
+// function changeSize() {
+//     const size = document.getElementById('fontSize').value;
+//     document.execCommand('fontSize', false, size);
+// }
 addNewDiv.addEventListener('click',()=>{
     const div=document.createElement('div');
     div.setAttribute('id','text-area');
     div.setAttribute('contenteditable','true');
     div.setAttribute('spellcheck','false');
     mainContent.appendChild(div);
+})
+document.addEventListener('keydown',(e)=>{
+    if(e.ctrlKey && e.key === "/"){ 
+        const div=document.createElement('div');
+        div.setAttribute('id','text-area');
+        div.setAttribute('contenteditable','true');
+        div.setAttribute('spellcheck','false');
+        mainContent.appendChild(div);
+    }
 })
 const editingButton=document.querySelector('.editing-button');
 const editingButton2=document.querySelector('.editing-button2');
@@ -175,6 +222,10 @@ selectBtn.addEventListener('click',()=>{
 })
 selectBtn2.addEventListener('click',()=>{
     showHide2.classList.toggle("show-edit");
+})
+imageButton.addEventListener('click',()=>{
+    document.querySelector('.image-button-div').classList.toggle('active');
+    imageDropdown.classList.toggle('show-edit');
 })
 options.forEach((option)=>{
     // console.log(option);
